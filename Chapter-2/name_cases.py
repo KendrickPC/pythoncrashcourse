b60fd9093d6e13b01191df213f307b3e405e85f5
KendrickPC <kennethpchang@gmail.com>
2018-04-11 21:59:07 +0800

    numbers section finished

--- Chapter-2/name_cases.py
+++ Chapter-2/name_cases.py
@@ -7,3 +7,26 @@ message = "Hello" + " " + first_name + ", " + question
 
 print(message)
 
+# 2-4 Name Cases
+full_name = "Kenderson Chang"
+print(full_name.lower())
+print(full_name.upper())
+print(full_name.title())
+
+# 2-5 Famous Quote
+quote = 'Some anonymous person once said "You will continue to suffer if you have an emotional reaction to everything that is said to you. \nTrue power is sitting back and observing everything with logic; true power is restraint. \nIf words control you that means everyone else can control you; breathe and allow things to pass." \n\t-Anonymous'
+print(quote)
+
+
+# 2-6 Famous Quote 2
+famous_person = "Anonymous"
+message = "You will continue to suffer if you have an emotional reaction to everything that is said to you. \nTrue power is sitting back and observing everything with logic; true power is restraint. \nIf words control you that means everyone else can control you; breathe and allow things to pass."
+print(message + " -" + famous_person)
+
+# 2-7 Stripping Names
+bigBaller1 = " Lonzo Ball"
+bigBaller2 = "LiAngelo Ball "
+bigBaller3 = " LaMelo Ball "
+print(bigBaller1, bigBaller2, bigBaller3)
+
+print(bigBaller1.lstrip(), bigBaller2.rstrip(), bigBaller3.strip())
---
+++ Chapter-2/numbers.py
@@ -0,0 +1,28 @@
+# integers
+a = 2 + 3
+print(a)
+
+b = 3 - 2
+print(b)
+
+c = 2 * 3
+print(c)
+
+d = 3 / 2
+print(d)
+
+# square root
+z = 3 ** 2
+print(z)
+
+y = 3 ** 3
+print(y)
+
+x = 10 ** 6
+print(x)
+
+w = 2 + 3 * 4
+print(w)
+
+v = (2 + 3) * 4
+print(v)
