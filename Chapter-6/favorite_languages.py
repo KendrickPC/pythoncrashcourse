f38540d56fa2c8cfe931ad7b12f4d87fbf1a68eb
KendrickPC <kennethpchang@gmail.com>
2018-05-02 18:52:56 +0800

    many_users.py file added to Chapter-6

--- Chapter-6/favorite_languages.py
+++ Chapter-6/favorite_languages.py
@@ -1,10 +1,16 @@
 favorite_languages = {
-	'jen': 'python',
-	'sarah': 'c',
-	'edward': 'ruby',
-	'phil': 'python',
-}
+	'jen': ['python', 'ruby'],
+	'sarah': ['c'],
+	'edward': ['ruby', 'go'],
+	'phil': ['python', 'haskell'],
+	}
 
-print("Sarah's favorite language is " + 
-	favorite_languages['sarah'].title() +
-	".")
+for name, languages in favorite_languages.items():
+	print("\n" + name.title() + "'s favorite languages are:")
+	for language in languages:
+		print("\t" + language.title())
+
+# You should not nest lists and dictionaries too deeply. If you are 
+# nesting items much deeper than what you see in the preceding examples
+# or you are working with another code with significant levels 
+# of nesting, most likely a simpler way to solve the problem exists.
