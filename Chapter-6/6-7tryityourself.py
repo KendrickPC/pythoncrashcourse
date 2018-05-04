17bd04bba79a9edd8434ecc3e9877b044d904404
KendrickPC <kennethpchang@gmail.com>
2018-05-04 19:24:30 +0800

    6-7 and 6-8 exercises completed

--- Chapter-6/6-7tryityourself.py
+++ Chapter-6/6-7tryityourself.py
@@ -11,8 +11,31 @@ person = {
 
 people.append(person)
 
+person = {
+	'first_name': 'brown',
+	'last_name': 'bear',
+	'age': 8,
+	'city': 'taipei city',
+	'username': 'brownbear',
+}
+
+people.append(person)
+
+person = {
+	'first_name': 'cony',
+	'last_name': 'rabbit',
+	'age': 8,
+	'city': 'taipei city',
+	'username': 'conyrabbit',
+}
+
+people.append(person)
 
 for person in people:
-	full_name = person['first_name'].title() + " " + person['last_name'].title() + "'s"
-	
-print(full_name)
+    name = person['first_name'].title() + " " + person['last_name'].title()
+    age = str(person['age'])
+    city = person['city'].title()
+    usernameID = person['username']
+	# print statement is inside the for loop    
+    print(name + ", of " + city + ", is " + age + " years old." + " Their username is: " + usernameID)
+
--- Chapter-6/6-8tryityourself.py
+++ Chapter-6/6-8tryityourself.py
@@ -1,1 +1,51 @@
-6-8tryityourself.py
+ # Pets: Make several dictionaries, where the name of each dictionary is the name of a pet.
+ # In each dictionary, include the kind of animal and the owners name. 
+ # Store these dictionaries in a list called pets . Next, loop through your list and
+ # as you do print everything you know about each pet.
+
+# empty list called pets
+pets = []
+
+pet = {
+	'animal_type': 'dog',
+	'name': 'abigail',
+	'sex': 'female',
+	'breed': 'border collie',
+	'owner': 'kenneth p chang',
+}
+pets.append(pet)
+
+pet = {
+	'animal_type': 'dog',
+	'name': 'perkins',
+	'sex': 'female',
+	'breed': 'corgi',
+	'owner': 'kenneth p chang',
+}
+pets.append(pet)
+
+pet = {
+	'animal_type': 'cat',
+	'name': 'buddy',
+	'sex': 'male',
+	'breed': 'mixed',
+	'owner': 'david son ngo',
+}
+pets.append(pet)
+
+# Testing for breaks
+# print(pets)
+
+for pet in pets: 
+	pet_info = pet['animal_type']
+	master = pet['owner']
+	pet_name = pet['name']
+	pet_breed = pet['breed']
+	pet_sex = pet['sex']
+
+	print("The " + pet_info + "'s owner is " + master.title() + "." \
+		+ " The " + pet_info + "'s " + "name is " + pet_name.title() + ". " \
+		+ "The pet's breed is " + pet_breed + " and is a "+ pet_sex)
+
+
+
