8e881f04b280190130b8fbc112848abda6b940dc
KendrickPC <kennethpchang@gmail.com>
2018-05-05 18:44:44 +0800

    6-11tryityourself.py cities exercise completed.

--- Chapter-6/6-10tryityourself.py
+++ Chapter-6/6-10tryityourself.py
@@ -1,1 +1,17 @@
-6-10tryityourself.py
+# 6-10. Favorite Numbers: Modify your program from 
+# Exercise 6-2 (page 102) so each person can have more
+# than one favorite number. Then print each persons 
+# name along with their favorite numbers.
+
+
+favorite_number = {
+	'kobe': [8, 24],
+	'jordan': [23, 6],
+	'lebron': [23, 1],
+}
+
+for name , numbers in favorite_number.items():
+	print(name.title() + "'s favorite numbers are:")
+	for number in numbers:
+		print(number)
+
--- Chapter-6/6-11tryityourself.py
+++ Chapter-6/6-11tryityourself.py
@@ -1,1 +1,31 @@
-6-11tryityourself.py
+# 6-11. Cities: Make a dictionary called cities . Use the names of three cities as keys in your dictionary . Create a dictionary of information about each city and include the country that the city is in, its approximate population, and one fact about that city . The keys for each citys dictionary should be something like country, population, and fact . Print the name of each city and all of the infor- mation you have stored about it .
+
+
+# 6-11 Cities
+cities = {
+	'los angeles': {
+		'country': 'united states',
+		'population': 4000000,
+		'fact': 'making movies',
+	},
+	'san francisco': {
+		'country': 'united states',
+		'population': 870000,
+		'fact': 'rent being really expensive',
+	},
+	'cupertino': {
+		'country': 'united states',
+		'population': 60700,
+		'fact': 'the hometown of kendrickpc'
+	},
+}
+
+for city, city_information in cities.items():
+	country_of_city = city_information['country']
+	population_of_city = str(city_information['population'])
+	fact_of_city = city_information['fact']
+	print("\tThe city of " + city.title() + \
+		", located in the country of the " + \
+		country_of_city.title() + \
+		", has a population of " + population_of_city \
+		+ " and is known for " + fact_of_city + ".")
