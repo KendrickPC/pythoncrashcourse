da47d68e590b3303497668d80496af8b56c90dd3
KendrickPC <kennethpchang@gmail.com>
2018-05-17 10:09:34 +0800

    9-3 tryityourself completed

--- Chapter-9/9-3tryityourself.py
+++ Chapter-9/9-3tryityourself.py
@@ -7,3 +7,34 @@
 # prints a personalized greeting to the user.
 # Create several instances representing different users, and call 
 # both methods for each user.
+
+
+class User(object):
+    def __init__(self, first_name, last_name, username, email, location_city):
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.username = username
+        self.email = email
+        self.location_city = location_city.title()
+
+    def describe_user(self):
+        print("\n" + self.first_name + " " +self.last_name)
+        print("  Username: " + self.username)
+        print("  Email: " + self.email)
+        print("  Location: " + self.location_city)
+
+    def greet_user(self):
+        message = "\nWelcome "+ self.first_name + " " + self.last_name
+        print(message)
+
+kendersonChang = User('kenneth', 'chang', 'kendrickpc', 'kennethpchang@gmail.com', 'taipei')
+kendersonChang.describe_user()
+kendersonChang.greet_user()
+
+robbieDoyen = User('robbie', 'doyen', 'doyenrobbie', 'doyen@codetaiwan.org', 'taipei')
+robbieDoyen.describe_user()
+robbieDoyen.greet_user()
+
+woodrowChen = User('woody', 'chen', 'woodrow', 'woodrow@codetaiwan.org', 'taipei')
+woodrowChen.describe_user()
+woodrowChen.greet_user()
