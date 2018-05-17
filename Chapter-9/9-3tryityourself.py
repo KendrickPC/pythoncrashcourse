5213f4eca0a834f7357c59344cd5b1b787db25cf
KendrickPC <kennethpchang@gmail.com>
2018-05-17 10:14:39 +0800

    9-3tryityourself.py checked with pep8

--- Chapter-9/9-3tryityourself.py
+++ Chapter-9/9-3tryityourself.py
@@ -1,11 +1,11 @@
 # 9-3 Users:
-# Make a class called User. Create two attributes called first_name 
-# and last_name, and then create several other attributes that are 
-# typically stored in a user profile. 
-# Make a method called describe_user() that prints a summary of the 
-# users information. Make another method called greet_user() that 
+# Make a class called User. Create two attributes called first_name
+# and last_name, and then create several other attributes that are
+# typically stored in a user profile.
+# Make a method called describe_user() that prints a summary of the
+# users information. Make another method called greet_user() that
 # prints a personalized greeting to the user.
-# Create several instances representing different users, and call 
+# Create several instances representing different users, and call
 # both methods for each user.
 
 
@@ -18,23 +18,26 @@ class User(object):
         self.location_city = location_city.title()
 
     def describe_user(self):
-        print("\n" + self.first_name + " " +self.last_name)
+        print("\n" + self.first_name + " " + self.last_name)
         print("  Username: " + self.username)
         print("  Email: " + self.email)
         print("  Location: " + self.location_city)
 
     def greet_user(self):
-        message = "\nWelcome "+ self.first_name + " " + self.last_name
+        message = "\nWelcome " + self.first_name + " " + self.last_name
         print(message)
 
-kendersonChang = User('kenneth', 'chang', 'kendrickpc', 'kennethpchang@gmail.com', 'taipei')
+kendersonChang = User('kenneth', 'chang', 'kendrickpc',
+                      'kennethpchang@gmail.com', 'taipei')
 kendersonChang.describe_user()
 kendersonChang.greet_user()
 
-robbieDoyen = User('robbie', 'doyen', 'doyenrobbie', 'doyen@codetaiwan.org', 'taipei')
+robbieDoyen = User('robbie', 'doyen', 'doyenrobbie',
+                   'doyen@codetaiwan.org', 'taipei')
 robbieDoyen.describe_user()
 robbieDoyen.greet_user()
 
-woodrowChen = User('woody', 'chen', 'woodrow', 'woodrow@codetaiwan.org', 'taipei')
+woodrowChen = User('woody', 'chen', 'woodrow',
+                   'woodrow@codetaiwan.org', 'taipei')
 woodrowChen.describe_user()
 woodrowChen.greet_user()
