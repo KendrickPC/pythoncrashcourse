a4b93ff40b881018eeb9d2ca495d1f7bbb7bad76
KendrickPC <kennethpchang@gmail.com>
2018-06-09 11:21:29 +0800

    Chapter-10 starteD

---
+++ Chapter-10/file_reader.py
@@ -0,0 +1,6 @@
+# Opens and reads txt files
+
+
+with open('pi_digits.txt') as file_object:
+    contents = file_object.read()
+    print(contents.rstrip())
---
+++ file_reader_root.py
@@ -0,0 +1,10 @@
+# Opens and reads txt files
+
+# Windows systems will sometimes interpret forward slashes in file paths
+# correctly. If you’re using Windows and you’re not getting the results you
+# expect, make sure you try using backslashes.
+
+
+with open('Chapter-10/pi_digits.txt') as file_object:
+    contents = file_object.read()
+    print(contents.rstrip())
