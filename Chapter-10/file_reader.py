58dd8ad27ccee6ea7084cebee946c3a8114eb018
KendrickPC <kennethpchang@gmail.com>
2018-06-09 11:32:13 +0800

    pi_digits.txt file working properly

--- Chapter-10/file_reader.py
+++ Chapter-10/file_reader.py
@@ -1,6 +1,21 @@
 # Opens and reads txt files
 
+# Windows systems will sometimes interpret forward slashes in file paths
+# correctly. If you are using Windows and you are not getting the results you
+# expect, make sure you try using backslashes.
 
-with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-    print(contents.rstrip())
+
+filename = 'pi_digits.txt'
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+# for line in lines:
+#     print(line.rstrip())
+
+pi_string = ''
+for line in lines:
+    pi_string +=line.rstrip()
+
+print(pi_string)
+print(len(pi_string))
